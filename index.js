23d98b90d6447a51e7b476b6a39f26318988ef6e
boris <boris@verhovs.ky>
2017-12-13 09:05:54 -0700

    comment elementsToIgnore

--- index.js
+++ index.js
@@ -2,26 +2,39 @@ let searchBarSelector = '#lst-ib';
 let searchBar = document.querySelector(searchBarSelector);
 searchBar.tabIndex = -1;
 
-// googleLogo, homepageLink, extraActionsDropDown, secondaryWebpageLinks,
-// webpagePathElements, videoThumbnails, imageThumbnails.
-// not sure if homepageLink is still necessary
-let elementsToIgonre = ['[aria-label="Google Search"]', '[title="Go to Google Home"]',
-                        'a.ab_button', 'a.fl', 'cite._Rm.bc > a', 'div.th._lyb._YQd > a',
-                        'div.bicc > a'].concat([searchBarSelector]);
+var new_element = searchBar.cloneNode(true);
+searchBar.parentNode.replaceChild(new_element, searchBar);
+
+let elementsToIgonre = [
+    // logo
+    '[aria-label="Google Search"]',
+    // homepage link, not sure if still necessary
+    '[title="Go to Google Home"]',
+    // extra actions drop down
+    'a.ab_button',
+    // secondary webpage links
+    'a.fl',
+    // webpage path elements
+    'cite._Rm.bc > a',
+    // video thumbnails
+    'div.th._lyb._YQd > a',
+    // image thumbnails
+    'div.bicc > a'
+];
 for (let selector of elementsToIgonre) {
     for (let elem of document.querySelectorAll(selector)) {
-        elem.tabIndex -= 1;
+        elem.tabIndex = '-1';
     }
 }
 
-let resultsSelector = '.r > a'
-let links = document.querySelectorAll(resultsSelector)
+let resultsSelector = '.r > a';
+let links = document.querySelectorAll(resultsSelector);
 
 let currentLinkIndex = 0;
 links[currentLinkIndex].focus();
 
 document.onkeypress = (e) => {
-    if (document.activeElement === searchBar) { return }
+    if (document.activeElement === searchBar) { return; }
 
     e.preventDefault();
 
@@ -49,7 +62,7 @@ document.onkeypress = (e) => {
             // put the cursor at the end of the input field instead of
             // the beginning
             searchBar.setSelectionRange(
-                searchBar.value.length, searchBar.value.length)
+                searchBar.value.length, searchBar.value.length);
             break;
 
         case nKey:
